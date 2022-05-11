2ce6ea2f8dab66f951b11079cdc60455cde8229e
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-05-11 22:33:27 +0300

    fix tests, add Report.md fnd Checklist.md

--- src/test/java/ru/netology/SendForm.java
+++ src/test/java/ru/netology/SendForm.java
@@ -1,5 +1,9 @@
 package ru.netology;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
@@ -11,6 +15,16 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SendForm {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     public void openBrowser() {
--- src/test/java/ru/netology/validationFieldTest/ValidationFieldMonthTest.java
+++ src/test/java/ru/netology/validationFieldTest/ValidationFieldMonthTest.java
@@ -1,5 +1,9 @@
-package ru.netology.validationFietd;
+package ru.netology.validationFieldTest;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.Page;
@@ -8,6 +12,16 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidationFieldMonthTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     public void openBrowser() {
